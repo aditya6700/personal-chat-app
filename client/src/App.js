def331a445fd423279a81fd2e2e7687bdcd2c8d6
aditya6700 <dsaiaditya@gmail.com>
2023-05-22 08:15:10 +0530

    client initial setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+};
