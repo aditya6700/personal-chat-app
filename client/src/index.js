dff91c216168eb365fe81cd7422c1125a7b92d89
aditya6700 <dsaiaditya@gmail.com>
2023-05-22 08:15:10 +0530

    client initial setup

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const Root = ReactDOM.createRoot(document.getElementById('root'));
+
+Root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
