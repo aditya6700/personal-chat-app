317f4e431b486689fa608e763016f4d5aaec29d8
aditya6700 <dsaiaditya@gmail.com>
2023-05-23 10:27:09 +0530

    Registration form complete with server connection

---
+++ client/src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const proxy = {
+    target: 'http://localhost:1432',
+    chnageOrigin: true
+}
+
+module.exports = function (app) {
+    app.use(
+        '/api/user',
+        createProxyMiddleware(proxy)
+    );
+};
