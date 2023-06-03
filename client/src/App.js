8b92307d73cd450d6006c953067c009178b0fd74
Sai Aditya Devulapalli <46639944+aditya6700@users.noreply.github.com>
2023-06-03 17:22:35 +0530

    Revert "Registration form complete with server connection"
    This reverts commit 317f4e431b486689fa608e763016f4d5aaec29d8.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Error from './pages/Error';
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/'         element={<Home />} />
-                <Route path='/login'    element={<Login />} />
-                <Route path='/register' element={<Register />} />
-                <Route path='*'         element={<Error />} />
-            </Routes>
-        </BrowserRouter>
+      <div className="App">
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "react-toastify/dist/ReactToastify.css";
 
 const Root = ReactDOM.createRoot(document.getElementById('root'));
 
