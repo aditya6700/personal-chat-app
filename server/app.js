3de369e20d4f039118efb2488109c77edc6fc9bb
aditya6700 <dsaiaditya@gmail.com>
2023-07-01 10:41:26 +0530

    socket connection at server

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 const express = require('express');
 const routes = require('./src/routes/routes');
 const cookieParser = require('cookie-parser');
+const socket = require('socket.io');
 
 dotenv.config({ path: './config.env' }); // configuring env file
 require('./src/db/conn'); // connecting to the database
@@ -34,6 +35,45 @@ app.post('*', (req, res) => {
 
 
 // setting up server to listen on a port
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Listening on port : ${PORT}`);
+});
+
+// setting up socket to listen to client
+const io = socket(server, {
+    cors: {
+        origin: "http://localhost:3000/",
+        credentials: true
+    },
+    path: '/socket.io'
+});
+
+// creating a global map to store online users
+global.onlineUsers = new Map();
+
+
+// socket to listen connection
+io.on('connection', (socket) => {
+    // store the current socket to chatsocket
+    global.chatSocket = socket;
+
+    // on add-user even is emitted from client
+    // store the current userid and the socket id in the map
+    socket.on('add-user', (userId) => {
+        onlineUsers.set(userId, socket.id);
+    });
+
+    // on send-msg event from client
+    socket.on('send-msg', (data) => {
+        // fetch the socketid of the user who sent the msg
+        const sendUserSocket = onlineUsers.get(data.to);
+
+        // if the user is online emit the msg-recieved
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit('msg-recieve', {
+                message: data.message,
+                time: data.time
+            });
+        };
+    });
 });
